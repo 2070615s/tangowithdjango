ac15c1d4e9a9e1fb6723c48d41ea828cc671e0c0
Christian Shtarkov <cshtarkov@gmail.com>
2015-01-12 14:23:04 +0000

    Chapter 4 - completed

--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world from Rango!")
+    content =   """Hello world from Rango!
+                <br/><a href=\"/rango/about/\">About</a>"""
+    return HttpResponse(content)
+
+def about(request):
+    content =   """This tutorial has been put together by Christian Shtarkov, 2070615
+                <br/><a href=\"/rango/\">Main</a>"""
+    return HttpResponse(content)
 
