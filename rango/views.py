6087fc078ea06fe2be940e5f565b901d3ddebe15
Christian Shtarkov <cshtarkov@gmail.com>
2015-01-20 17:48:32 +0000

    Chapter 5 - completed

--- rango/views.py
+++ rango/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    content =   """Hello world from Rango!
-                <br/><a href=\"/rango/about/\">About</a>"""
-    return HttpResponse(content)
+    context_dict = {"boldmessage": "I am bold font from the context."}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    content =   """This tutorial has been put together by Christian Shtarkov, 2070615
-                <br/><a href=\"/rango/\">Main</a>"""
-    return HttpResponse(content)
-
+    return render(request, 'rango/about.html', {})
