203daa24c6795520a1ab998ffcec38ddf450c453
Christian Shtarkov <cshtarkov@gmail.com>
2015-03-08 18:53:37 +0000

    Filtering categories

--- rango/keys.py
+++ rango/keys.py
@@ -1,2 +1,2 @@
-BING_API_KEY = ''
+BING_API_KEY = '3tupGPB44pfUWnGGea7xxuvMOfEUx8CVl9tb3+wmqqg'
 
--- static/js/rango-ajax.js
+++ static/js/rango-ajax.js
@@ -7,3 +7,11 @@ $('#likes').click(function(){
     });
 });
 
+$('#suggestion').keyup(function(){
+        var query;
+        query = $(this).val();
+        $.get('/rango/suggest_category/', {suggestion: query}, function(data){
+         $('#cats').html(data);
+        });
+});
+
